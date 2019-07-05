9994493e05bc6cee559f6683e99bb8d49ad8c1fc
Fábio Henrique Gabriele <fabiohenriqueg@gmail.com>
2019-07-05 08:03:29 -0300

    - Simplified the logic

--- src/join2Arrays.ts
+++ src/join2Arrays.ts
@@ -1,21 +1,5 @@
 import { sortArrayAsc } from './sortArrayAsc'
 
 export const join2Arrays = function (arr1: number[], arr2: number[]): number[] {
-  let ret: number[] = [];
-  const arr1Ordered = sortArrayAsc(arr1);
-  const arr2Ordered = sortArrayAsc(arr2);
-
-  if (arr1Ordered.length > arr2Ordered.length) {
-    arr2Ordered.forEach(e => {
-      if (arr1Ordered.indexOf(e) !== -1 && !(ret.indexOf(e) > -1))
-        ret.push(e);
-    });
-  } else {
-    arr1Ordered.forEach(e => {
-      if (arr2Ordered.indexOf(e) !== -1 && !(ret.indexOf(e) > -1))
-        ret.push(e);
-    });
-  }
-
-  return ret;
+  return sortArrayAsc(arr1.filter((e, i, array) => (arr2.indexOf(e) !== -1)));
 };
